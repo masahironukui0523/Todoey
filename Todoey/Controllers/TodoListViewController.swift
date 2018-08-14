042521dc981514f4e5f3332768431d715d34ebe7
masa <mickey24352002@gmail.com>
2018-08-14 09:35:50 +0900

    Convert to MVC design pattern.

--- Todoey/Controllers/TodoListViewController.swift
+++ Todoey/Controllers/TodoListViewController.swift
@@ -10,43 +10,68 @@ import UIKit
 
 class TodoListViewController: UITableViewController {
     
-    // 前から順番にセルに入る
-    var itemArray = ["宿題をする", "牛乳を買う", "手紙を書く" ]
-
+    var itemArray = [Item]()
     let defaults = UserDefaults.standard
     
-    // MARK - セルの数を指定
+    
+    
+    override func viewDidLoad() {
+        
+        super.viewDidLoad()
+        
+        // NaviBarのタイトルを大きく表示させる
+        navigationController?.navigationBar.prefersLargeTitles = true
+        
+        let newItem = Item()
+        newItem.title = "宿題をする"
+        itemArray.append(newItem)
+        
+        let newItem2 = Item()
+        newItem2.title = "牛乳を買う"
+        itemArray.append(newItem2)
+        
+        let newItem3 = Item()
+        newItem3.title = "手紙を書く"
+        itemArray.append(newItem3)
+        
+        
+        
+        // defaultsからデータの取り出し
+        if let items = defaults.array(forKey: "TodoListArray") as? [Item] {
+            itemArray = items
+        }
+        
+    }
+    
+    
+    
+    // MARK - TableViewデータソースメソッド
+    
     override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
         
+        // セルの数を指定
         return itemArray.count
         
     }
     
-    
-    override func viewDidLoad() {
-        super.viewDidLoad()
-        
-        // タイトルの大きさを変更
-        navigationController?.navigationBar.prefersLargeTitles = true
-        
-        // defaultsからデータの取り出し
-        if let items = defaults.array(forKey: "TodoListArray") as? [String] {
-            itemArray = items
-        }
-        
-        
-    }
-    
-    
-    
-    // MARK - セルのカスタマイズ
     override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
         
         let cell = tableView.dequeueReusableCell(withIdentifier: "ToDoItemCell", for: indexPath)
-        cell.textLabel?.text = itemArray[indexPath.row]
+        
+        let item = itemArray[indexPath.row]
+        
+        cell.textLabel?.text = item.title
+        
+        // チェックマーク
+        // Ternary operator
+        // value = 条件 ? tureの場合の処理 : falseの場合の処理
+        cell.accessoryType = item.done ? .checkmark : .none
+        
+        
         return cell
         
     }
+    
     
     
     
@@ -56,39 +81,39 @@ class TodoListViewController: UITableViewController {
         
         let cell = tableView.cellForRow(at: indexPath)
         
-        // チェックマーク
-        if cell?.accessoryType == .checkmark{
-            cell?.accessoryType = .none
-        } else {
-            cell?.accessoryType = .checkmark
-        }
+        itemArray[indexPath.row].done = !itemArray[indexPath.row].done
         
-        // 選択を外されたセルに実行される処理
+        // マジカルメソッド！！　TableViewデータソースメソッドを呼び出しセルにチェックマークを反映
+        tableView.reloadData()
+        
+        // セルを選択した時の背景の変化を遅くする
         tableView.deselectRow(at: indexPath, animated: true)
         
     }
     
     
     
-    // MARK - アイテム追加機能
+    // MARK - 新規アイテム追加機能
     
     @IBAction func addButtonPressed(_ sender: Any) {
         // プラスボタンが押された時に実行される処理
         
         var textField = UITextField()
         
-        let alert = UIAlertController(title: "新しいリストを追加", message: "", preferredStyle: .alert)
+        let alert = UIAlertController(title: "新しいアイテムを追加", message: "", preferredStyle: .alert)
         
         let action = UIAlertAction(title: "リストに追加", style: .default) { (action) in
             // 「リストに追加」を押された時に実行される処理
             
-            // 配列に追加
-            self.itemArray.append(textField.text!)
+            let newItem = Item()
+            newItem.title = textField.text!
+            
+            // アイテム追加処理
+            self.itemArray.append(newItem)
             
             // defaultsにデータを保存
             self.defaults.set(self.itemArray, forKey: "TodoListArray")
             
-            // 更新
             self.tableView.reloadData()
             
         }
@@ -105,15 +130,16 @@ class TodoListViewController: UITableViewController {
     
     
     
-    // MARK - アイテム削除機能
+    // MARK - スワイプでのアイテム削除機能
     override func tableView(_ tableView: UITableView, commit editingStyle: UITableViewCellEditingStyle, forRowAt indexPath: IndexPath) {
         
-        // 配列から削除
+        // アイテム削除処理
         itemArray.remove(at: indexPath.row)
-        // 更新
-        tableView.reloadData()
+        let indexPaths = [indexPath]
+        tableView.deleteRows(at: indexPaths, with: .automatic)
         
     }
+
     
 }
 
---
+++ Todoey/Data Model/item.swift
@@ -0,0 +1,15 @@
+//
+//  item.swift
+//  Todoey
+//
+//  Created by 抜井正寛 on 2018/08/14.
+//  Copyright © 2018 Masahiro Nukui. All rights reserved.
+//
+
+import Foundation
+
+// ファイル名と同じクラス名にする
+class Item {
+    var title : String = ""
+    var done: Bool = false
+}
